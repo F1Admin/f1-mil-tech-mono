0d1781f8f6125d15161aa416b50c11cbfb24a169
Ian Mitchard <ian.mitchard@gmail.com>
2024-08-16 15:10:01 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # f1-mil-tech-mono
+This is a mono repo for Flight-1.com's Military and Tech Sites. CMS provided by Sanity. 
---
+++ next/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Military and Tech Project</h1>
+    </div>
+  );
+}
