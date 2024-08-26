0225f1a927de02a784d6b72b8ddbaf1a7d8595d0
Ian Mitchard <ian.mitchard@gmail.com>
2024-08-26 17:49:04 -0600

    military redirect

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,18 +1,5 @@
-import React from 'react';
-import Link from 'next/link';
-
 const LandingPage: React.FC = () => {
-  return (
-    // to buttons with links the /military and /tech
-    <div className="flex flex-col p-5 gap-3">
-      <Link href={'/military'} className="text-center border">
-        Military
-      </Link>
-      <Link href={'/tech'} className="text-center border">
-        Tech
-      </Link>
-    </div>
-  );
+  return <div>Flight-1 Military - Redirected</div>;
 };
 
 export default LandingPage;
