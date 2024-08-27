a43228face52833dfd5968f9cf94e1b5b5cb1c74
Ian Mitchard <ian.mitchard@gmail.com>
2024-08-27 12:42:38 -0600

    responsive footer

--- next/app/components/Footer.tsx
+++ next/app/components/Footer.tsx
@@ -16,59 +16,71 @@ export default async function Footer() {
     <footer>
       <div className="mx-auto py-10 px-10 text-zinc-400">
         <hr className="border-t-2 border-zinc-400 mb-10" />
-        <div className="grid grid-cols-5 px-10 mb-20">
-          <div className="flex flex-col gap-10">
-            <div className="">FLIGHT-1 MILITARY</div>
-            <div className="flex flex-col gap-2">
-              {militaryLinks
-                .filter((item) => item.label != 'Courses')
-                .map((link, index) => (
+        <div className="flex items-start justify-between">
+          <div className="md:grid lg:grid-cols-4 xl:grid-cols-5 lg:px-5 xl:px-10 mb-20">
+            <div className="flex flex-col gap-3 lg:gap-10 mb-10">
+              <div className="">FLIGHT-1 MILITARY</div>
+              <div className="flex flex-col gap-2">
+                {militaryLinks
+                  .filter((item) => item.label != 'Courses')
+                  .map((link, index) => (
+                    <Link
+                      key={index}
+                      href={link.path}
+                      className=" hover:text-white font-thin text-s capitalize"
+                    >
+                      {link.label}
+                    </Link>
+                  ))}
+              </div>
+            </div>
+            <div className="flex flex-col gap-3 lg:gap-10 mb-10">
+              <div>COURSES</div>
+              <ul className="flex flex-col gap-2">
+                {courses.map((course) => (
                   <Link
-                    key={index}
-                    href={link.path}
-                    className=" hover:text-white font-thin text-s capitalize"
+                    key={course._id}
+                    href={`/military/courses/${course.slug}`}
+                    className=" hover:text-white font-thin text-s capitalize whitespace-nowrap"
                   >
-                    {link.label}
+                    {course.courseNumber
+                      ? `${course.courseNumber}M ${course.courseTitle}`
+                      : course.courseTitle}
                   </Link>
                 ))}
+              </ul>
+            </div>
+            <div className="flex flex-col gap-3 lg:gap-10 mb-10">
+              <div className="flex whitespace-nowrap">
+                FLIGHT-1 SUPPORTING COURSES
+              </div>
+              <ul className="flex flex-col gap-2">
+                {supportingCourses.map((course) => (
+                  <Link
+                    key={course._id}
+                    href={`/military/courses/supporting-courses/${course.slug}`}
+                    className=" hover:text-white font-thin text-s capitalize"
+                  >
+                    {course.courseTitle}
+                  </Link>
+                ))}
+              </ul>
+            </div>
+            <div className="hidden lg:flex relative w-80 h-80 lg:col-end-5 xl:col-end-6">
+              <Image
+                src={logo.footerLogo}
+                alt="Flight 1 Skull"
+                fill
+                className="object-contain opacity-30"
+                priority
+              />
             </div>
           </div>
-          <div className="flex flex-col gap-10">
-            <div>COURSES</div>
-            <ul className="flex flex-col gap-2">
-              {courses.map((course) => (
-                <Link
-                  key={course._id}
-                  href={`/military/courses/${course.slug}`}
-                  className=" hover:text-white font-thin text-s capitalize"
-                >
-                  {course.courseNumber
-                    ? `${course.courseNumber}M ${course.courseTitle}`
-                    : course.courseTitle}
-                </Link>
-              ))}
-            </ul>
-          </div>
-          <div className="flex flex-col gap-10">
-            <div>FLIGHT-1 SUPPORTING COURSES</div>
-            <ul className="flex flex-col gap-2">
-              {supportingCourses.map((course) => (
-                <Link
-                  key={course._id}
-                  href={`/military/courses/supporting-courses/${course.slug}`}
-                  className=" hover:text-white font-thin text-s capitalize"
-                >
-                  {course.courseTitle}
-                </Link>
-              ))}
-            </ul>
-          </div>
-          <div className="flex col-end-6 justify-end">
+          <div className="relative w-72 h-72 sm:w-80 sm:h-80 md:w-96 md:h-96 lg:hidden">
             <Image
               src={logo.footerLogo}
               alt="Flight 1 Skull"
-              width={200}
-              height={1000}
+              fill
               className="object-contain opacity-30"
               priority
             />
