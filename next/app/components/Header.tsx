77bc4e0010f5b95d6a0ad0d74e42d55310a284e1
Ian Mitchard <ian.mitchard@gmail.com>
2024-08-27 11:58:29 -0600

    loading and header responsive

--- next/app/components/Header.tsx
+++ next/app/components/Header.tsx
@@ -8,10 +8,12 @@ import militaryLinks from '../data/militaryLinks';
 import baseLinks from '../data/baseLinks';
 import { usePathname } from 'next/navigation';
 import { getSiteSettings } from '@/sanity/sanity-utils';
+import path from 'path';
 
 const Header = () => {
   const [logo, setLogo] = useState('');
   const [loading, setLoading] = useState(true);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
   const pathname = usePathname();
 
@@ -29,7 +31,7 @@ const Header = () => {
 
   return (
     <header className="bg-black shadow-md text-zinc-400">
-      <div className="mx-auto py-[22px] px-11 flex justify-between items-center">
+      <div className="mx-auto px-4 py-[22px] lg:px-11 flex justify-between items-center">
         {/* Logo */}
         <Link href="/military">
           <Image
@@ -67,25 +69,60 @@ const Header = () => {
         </nav>
       </div>
       {/* Mobile Menu Icon */}
-      <div className="lg:hidden flex items-center">
+      <div className="lg:hidden flex justify-between bg-zinc-900">
         <button
-          onClick={() => setMenuOpen(!menuOpen)}
-          className="text-zinc-700 hover:text-zinc-900 focus:outline-none px-10 py-4 space-x-8"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+            setMobileMenuOpen(false);
+          }}
+          className="bg-zinc-900 text-zinc-200 hover:text-white focus:outline-none px-4 py-4 space-x-8 text-xs uppercase"
         >
-          <ChevronDown
-            transform={menuOpen ? 'rotate(-180deg)' : 'rotate(0deg)'}
-          />
+          <div className="flex items-center">
+            Military
+            <ChevronDown
+              transform={menuOpen ? 'rotate(-180deg)' : 'rotate(0deg)'}
+            />
+          </div>
+        </button>
+        <button
+          className="space-y-1 px-4 py-4 flex flex-col justify-center items-center"
+          onClick={() => {
+            setMobileMenuOpen(!mobileMenuOpen);
+            setMenuOpen(false);
+          }}
+        >
+          <div className="w-8 h-1 bg-zinc-400" />
+          <div className="w-8 h-1 bg-zinc-400" />
+          <div className="w-8 h-1 bg-zinc-400" />
         </button>
       </div>
-      {/* Mobile Menu */}
+      {/* Mobile Base Menu */}
       {menuOpen && (
-        <div className="lg:hidden bg-white shadow-md">
+        <div className="lg:hidden bg-zinc-600 shadow-md">
           <nav className="flex flex-col space-y-4 px-4 py-4">
-            {baseLinks.map((link, index) => (
+            {baseLinks
+              .filter((link) => link.path !== '/military')
+              .map((link, index) => (
+                <Link
+                  key={index}
+                  href={link.path}
+                  className="text-zinc-200 text-xs hover:text-white"
+                >
+                  {link.label}
+                </Link>
+              ))}
+          </nav>
+        </div>
+      )}
+      {/* Mobile Military Menu */}
+      {mobileMenuOpen && (
+        <div className="lg:hidden bg-zinc-600">
+          <nav className="flex flex-col space-y-4 px-4 py-4">
+            {militaryLinks.map((link, index) => (
               <Link
                 key={index}
-                href="/"
-                className="text-zinc-400 hover:text-white"
+                href={link.path}
+                className="text-zinc-200 uppercase text-xs hover:text-white"
               >
                 {link.label}
               </Link>
