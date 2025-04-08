5becd1c40046ae9809283aaa3529d15d023311ea
Ian Mitchard <ian.mitchard@gmail.com>
2025-04-07 21:40:53 -0600

    add heading styling to features

--- tech/app/(site)/products/[slug]/page.tsx
+++ tech/app/(site)/products/[slug]/page.tsx
@@ -77,6 +77,21 @@ export default async function ProductPage({ params }: { params: Params }) {
                 components={{
                   block: {
                     normal: ({ children }) => <div>{children}</div>,
+                    h1: ({ children }) => (
+                      <h1 className="text-4xl font-bold">{children}</h1>
+                    ),
+                    h2: ({ children }) => (
+                      <h2 className="text-3xl font-bold">{children}</h2>
+                    ),
+                    h3: ({ children }) => (
+                      <h3 className="text-2xl font-bold">{children}</h3>
+                    ),
+                    h4: ({ children }) => (
+                      <h4 className="text-xl font-bold">{children}</h4>
+                    ),
+                    h5: ({ children }) => (
+                      <h5 className="text-lg font-bold">{children}</h5>
+                    ),
                   },
                   list: {
                     bullet: ({ children }) => (
